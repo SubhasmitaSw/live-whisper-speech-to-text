625cf97b0e0351845e154810a095f5dbd7156f84
Subhasmita <subhasmitaswain232@gmail.com>
2023-08-23 20:44:05 +0530

    regulate and import pcm modules

--- main.js
+++ main.js
@@ -1,18 +1,76 @@
 import { WebVoiceProcessor } from '@picovoice/web-voice-processor';
+import { write } from 'fs';
 import httpx from 'httpx';
 
 const startButton = document.getElementById('start-button');
 const stopButton = document.getElementById('stop-button');
+const voiceProcessor = new WebVoiceProcessor();
 const whisperEndpoint = "http://31.28.93.84";
 
+let isRecording = false;
 
+// Function to load audio file and return it as an array
+const loadFile = async (inputPath) => {
+    const response = await fetch(inputPath);
+    const audioData = await response.arrayBuffer();
+    return new Float32Array(audioData);
+};
+
+// Function to send GET request to get the result
+const getResult = async (id) => {
+    const response = await httpx.get(`${whisperEndpoint}/result/${id}`);
+    return JSON.parse(response.body.toString());
+};
+
+// Function to send POST request to transcribe the audio
+const transcribe = async (input) => {
+    const response = await httpx.post(`${whisperEndpoint}/transcribe`, {
+        json: input
+    });
+    return JSON.parse(response.body.toString());
+};
+
+// Function to start recording
+const startRecording = () => {
+    if (!isRecording) {
+        voiceProcessor.start();
+        isRecording = true;
+    }
+};
 
 const engine = {
-    onmessage: function(e) {
+    onmessage: async function (e) {
         switch (e.data.command) {
             case 'process':
                 const inputData = e.data.inputFrame;
-                handleAudioData(inputData);
+                
+                // Convert Float32Array to regular array
+                const inputFeatures = Array.from(inputData);
+                
+                // Prepare the input for transcription
+                const transcriptionInput = {
+                    input_features: inputFeatures,
+                    data_pre_processing: true,
+                    data_post_processing: true,
+                    callback_url: 'https://webhook.site/dd542931-61ca-42aa-8584-6e11bfbcc0fa',
+                };
+
+                // Perform the transcription
+                const transcribeRequest = await transcribe(transcriptionInput);
+                console.log(transcribeRequest);
+
+                const taskId = transcribeRequest.task_id;
+                console.log(taskId);
+
+                // Get the result
+                const result = await getResult(taskId);
+                console.log(result);
+
+                // Handle the result as needed
+                // For example, you can display the transcribed text in the 'transcription-output' div:
+                const transcriptionOutput = document.getElementById('transcription-output');
+                transcriptionOutput.innerText = result.transcription;
+
                 break;
         }
     }
@@ -30,10 +88,9 @@ const handleAudioData = async (inputData) => {
     });
 
     // Display the transcribed text
-    const transcription = response.json().transcription;
+    const transcription = response.json()?.transcription;
     document.getElementById('transcription-output').innerText = transcription;
 };
-
 
 
 
